e5b662429d2feba9c7e16cbcfde301badf6e1d87
jakevercella <jakevercella@u.boisestate.edu>
2023-04-28 16:01:51 -0600

    random string

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -33,14 +33,14 @@ public class GettingStartedApplication {
     String database(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
-            statement.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp (tick timestamp, random_string varchar(30))");
+            statement.executeUpdate("INSERT INTO table_timestamp VALUES (now(), '" + getRandomString() + "')");
 
 
-            final var resultSet = statement.executeQuery("SELECT tick FROM table_timestamp_and_random_string");
+            final var resultSet = statement.executeQuery("SELECT tick FROM table_timestamp");
             final var output = new ArrayList<>();
             while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
+                output.add("Read from DB: " + resultSet.getTimestamp("tick") + resultSet.getString("tick"));
             }
 
             model.put("records", output);
