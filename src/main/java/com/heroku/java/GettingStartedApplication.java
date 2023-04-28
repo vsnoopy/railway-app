5e4316e58d019065d7ec16734293270285739dc0
jakevercella <jakevercella@u.boisestate.edu>
2023-04-28 15:51:13 -0600

    random string

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -58,11 +58,15 @@ public class GettingStartedApplication {
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
         Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
 
-        return random.ints(leftLimit, rightLimit + 1)
-                .limit(targetStringLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
+        return generatedString;
     }
 
     public static void main(String[] args) {
